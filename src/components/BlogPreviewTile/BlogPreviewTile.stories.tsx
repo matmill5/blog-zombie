980fba070ba82b9c532986d3f597e98cb6f03f74
Matthew E. Miller <mmill.personal@gmail.com>
2021-07-19 19:05:44 -0400

    Added placeholder image and further defined BlogPreviewTile

--- src/components/BlogPreviewTile/BlogPreviewTile.stories.tsx
+++ src/components/BlogPreviewTile/BlogPreviewTile.stories.tsx
@@ -3,6 +3,7 @@ import { Story, Meta } from "@storybook/react";
 import BlogPreviewTile from "./BlogPreviewTile";
 import { BlogPreviewTileProps } from "./types";
 import { blogPosts as testBlogPosts } from "../../../globalConstants";
+import BlogPhotoPlaceholder from "../../assets/jpg/blog-banner-image-placeholder.jpg";
 
 // BlogPreviewTile.stories.tsx
 //https://storybook.js.org/docs/react/writing-stories/introduction
@@ -23,12 +24,12 @@ const Template: Story<BlogPreviewTileProps> = (args) => (
 export const BlogPreviewTilePrimary = Template.bind({});
 BlogPreviewTilePrimary.args = {
   id: "blog preview tile",
-  title: aBlogPost.blogPostMeta.title,
-  subtitle: aBlogPost.blogPostMeta.subtitle,
-  author: aBlogPost.blogPostMeta.author,
-  dateCreated: aBlogPost.blogPostMeta.dateCreated,
-  bannerImage: aBlogPost.blogPostMeta.bannerImage,
-  url: aBlogPost.blogPostMeta.url,
+  title: "Say hello to Blog Zombie",
+  subtitle: "A BMS that isn't scary. Easily add blogs to your SPA/CRA.",
+  author: "Matthew E. Miller",
+  dateCreated: "07/19/2021",
+  bannerImage: BlogPhotoPlaceholder,
+  url: "/",
 };
 
 export const BlogPreviewTileSecondary = Template.bind({});
--- src/components/BlogPreviewTile/BlogPreviewTile.tsx
+++ src/components/BlogPreviewTile/BlogPreviewTile.tsx
@@ -3,30 +3,67 @@ import styled from "styled-components";
 import { BlogPreviewTileProps } from "./types";
 
 const BlogPreviewTileContainer = styled.div`
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  elevation: 4;
+  padding: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1);
+  position: relative;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  display: inline;
+`;
 
-const Subtitle = styled.h2``;
+const Subtitle = styled.h2`
+  display: inline;
+`;
 
-const BannerImage = styled.img``;
+const BannerImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
 
-const Author = styled.p``;
+const BannerImageContainer = styled.div`
+  height: 200px;
+  width: 100%;
+`;
 
-const DateCreated = styled.p``;
+const Author = styled.p`
+  display: inline;
+`;
 
-const Url = styled.a``;
+const DateCreated = styled.p`
+  display: inline;
+`;
 
+const Url = styled.a`
+  display: inline;
+`;
+
+const TileFooterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-flow: wrap;
+`;
 const BlogPreviewTile: FC<BlogPreviewTileProps> = ({ ...props }) => {
   return (
     <BlogPreviewTileContainer>
+      <BannerImageContainer>
+        <BannerImage src={props.bannerImage}></BannerImage>
+      </BannerImageContainer>
       <Title>{props.title}</Title>
       <Subtitle>{props.subtitle}</Subtitle>
-      <BannerImage src={props.bannerImage}></BannerImage>
-      <Author>{props.author}</Author>
-      <DateCreated>{props.dateCreated}</DateCreated>
-      <Url href={props.url}>{props.url}</Url>
+      <TileFooterContainer>
+        <Author>{props.author}</Author>
+        <DateCreated>{props.dateCreated}</DateCreated>
+        <Url href={props.url}>{props.url}</Url>
+      </TileFooterContainer>
     </BlogPreviewTileContainer>
   );
 };
